21d11727e50ce6ecc5710c3fc79225cce6caec31
Biruk Lopiso Adore <birukabay1212@gmail.com>
2024-07-18 09:52:04 -0700

    List Function

---
+++ List Functions.py
@@ -0,0 +1,14 @@
+List1 = [1,2,3,3,3,3,3,3,4]
+List2 = [1,2,3,4,5,6,7,8]
+List3 = ['John', 'Peter', 'Brook']
+List2.reverse()
+#List2.sort()
+#List2.remove(8)
+#List2.extend(List3)
+#List2.append(99)
+#List2.insert(3,44)
+#List2.pop()
+#List2.clear()
+
+
+print(List2)
